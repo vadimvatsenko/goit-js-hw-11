134e40a86bec1de384aa3091598576ec539f53ef
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-10-14 09:34:23 +0300

    14.10.2022 09:34

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { createMarkup } from './js/createMarkup';
 import SimpleLightbox from "simplelightbox";
+
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { getRefs } from './js/getRefs';
