ca9ecf5bdcaaf339bd94f6bfd7abd7d722b933b0
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-10-16 14:07:16 +0300

    16.10.2022 14:07

--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
+// import axios from 'axios';
+// import { createMarkup } from './js/createMarkup';// шаблон разметки
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+// import LoadMoreBtn from './js/loadMoreBtn';// импорт кнопки loadmorebtn
+// import { getRefs } from './js/getRefs';// импорт рефов
+// import NewAPI from './js/API'// импортирую класс
+// import { onScroll, onTopButton } from './js/scroll';
+// import { emptySearchMessage, noImagesFoundMessage, imagesFoundMessage, endOfSearchMessage } from './js/notify';
+
+// onScroll();//запуск скролла
+// onTopButton();
+// let lightbox;
+
+// const newApiService = new NewAPI();// экземпляр класса для получения методов и свойств
+// const refs = getRefs();// получаем рефы
+
+// refs.searchField.addEventListener('submit', handleSubmit);
+
+// function handleSubmit(e) {
+//     e.preventDefault();
+//     newApiService.query = e.currentTarget.searchQuery.value.trim();// записываем в query при помощи set введеную инвормацию в форму
+    
+
+//     if (newApiService.query === '') {
+//         clearMarkup();
+//         loadMoreBtn.hide();
+//         return emptySearchMessage();
+        
+//     }
+
+//     loadMoreBtn.show();//показать кнопку
+//     clearMarkup();//очистить разметку
+//     newApiService.resetPage(); // cбрасываем страницу на 1ю
+//     runSimpleLightBox();// запустить lightbox
+//     fetchAll();//
+
+    
+    
+// }
+
+// export const loadMoreBtn = new LoadMoreBtn({
+//     selector: '.load-more',
+//     hidden: true,
+// });// теперь реф для кнопки loadmore не нужен
+
+// loadMoreBtn.refs.button.addEventListener('click', onClickMoreBtn);// обращение к кнопке через класс
+
+// function onClickMoreBtn() {
+//     fetchAll();
+// }
+
+// function fetchAll() {
+//     loadMoreBtn.disable();
+//     newApiService.fetchArticles()
+//         .then(card => {
+            
+//             loadMoreBtn.enable();
+//             runMarkup(card); //функция разметки
+//             lightbox.refresh(); // обновить lightbox
+            
+//         });
+// }
+
+// function runMarkup(c) {// создание разметки
+//     refs.getGallery.insertAdjacentHTML('beforeend', createMarkup(c));
+// }
+
+// function clearMarkup() {// очистка разметки
+//     refs.getGallery.innerHTML = '';
+// }
+
+// function runSimpleLightBox() {
+//     lightbox = new SimpleLightbox('.gallery .gallery__link', {
+//         captionsData: 'alt',
+//         captionDelay: 250,
+//     });
+
+// }
+
+
 import axios from 'axios';
 import { createMarkup } from './js/createMarkup';// шаблон разметки
 import SimpleLightbox from "simplelightbox";
@@ -34,6 +115,7 @@ function handleSubmit(e) {
     newApiService.resetPage(); // cбрасываем страницу на 1ю
     runSimpleLightBox();// запустить lightbox
     fetchAll();//
+    
 
     
     
@@ -53,14 +135,28 @@ function onClickMoreBtn() {
 function fetchAll() {
     loadMoreBtn.disable();
     newApiService.fetchArticles()
-        .then(card => {
-            
-           
+        .then(data => {
+            console.log(data.totalHits)
+            if (data.totalHits === 0) {
+                noImagesFoundMessage();
+            }
+            if (newApiService.page === 2 && data.totalHits !== 0) {
+                imagesFoundMessage(data.totalHits);
+            }
+            if (newApiService.page - 1 !== 1) {
+                loadMoreBtn.hide();
+                endOfSearchMessage();
+
+            }
+            if (data.totalHits < 40) {
+                loadMoreBtn.hide();
+            }
+
             loadMoreBtn.enable();
-            runMarkup(card); //функция разметки
+            runMarkup(data.hits); //функция разметки
             lightbox.refresh(); // обновить lightbox
             
-        });
+    });
 }
 
 function runMarkup(c) {// создание разметки
--- src/js/API.js
+++ src/js/API.js
@@ -1,36 +1,85 @@
-
-import { emptySearchMessage, noImagesFoundMessage, imagesFoundMessage, endOfSearchMessage } from './notify';
+// import axios from 'axios';
+// import { emptySearchMessage, noImagesFoundMessage, imagesFoundMessage, endOfSearchMessage } from './notify';
 // import LoadMoreBtn from './loadMoreBtn';
 // import loadMoreBtn from '../index';
+
+// export default class NewApi {
+//     constructor() {
+//         this.searchQuery = ''// по умолчанию форма пуста, класс должен хранить эту информацию
+//         this.page = 1;// по умолчанию страница 1
+//     }// конструктор
+//     fetchArticles() {// ничего не будем получать
+//         const API_KEY = '29703536-3492bea623abb7896113a32cf';
+//         const BASE_URL = 'https://pixabay.com/api/';
+//         const SEARCH_SETTINGS = `image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
+//         const URL = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&${SEARCH_SETTINGS}`;
+
+//        return fetch(URL)// возращаем данные промиса во внешний код
+//             .then(response => response.json())
+//            .then(data => {
+//                console.log(data);
+//                if (this.page === 1 && data.totalHits !== 0 ) {
+//                    imagesFoundMessage(data.totalHits);
+//                }
+//                if (data.totalHits < 40) {
+//                   endOfSearchMessage();
+//                 //    loadMoreBtn.hide();
+
+//                }
+              
+
+            
+        
+//                this.incrementPage();// если запрос успешный то увеличиваем страницу на 1
+//                return data.hits ; //возвращаем данные промиса во внешний код
+//         });
+//     }
+  
+//     incrementPage() {
+//         this.page += 1;
+//     }
+//     resetPage() {
+//         this.page = 1;
+//     }
+//     get currentPage() {
+//         return this.page;
+//     }
+//     get query() {
+//         return this.searchQuery// если нужно будет получить значение query
+//     }
+
+//     set query(newQeury) {// если нужно будет обновить searchQuery
+//         this.searchQuery = newQeury;
+//     }
+
+// }
+
+import axios from 'axios';
+import { emptySearchMessage, noImagesFoundMessage, imagesFoundMessage, endOfSearchMessage } from './notify';
+import LoadMoreBtn from './loadMoreBtn';
+import loadMoreBtn from '../index';
 
 export default class NewApi {
     constructor() {
         this.searchQuery = ''// по умолчанию форма пуста, класс должен хранить эту информацию
         this.page = 1;// по умолчанию страница 1
     }// конструктор
-    fetchArticles() {// ничего не будем получать
+    async fetchArticles() {// ничего не будем получать
         const API_KEY = '29703536-3492bea623abb7896113a32cf';
         const BASE_URL = 'https://pixabay.com/api/';
         const SEARCH_SETTINGS = `image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
         const URL = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&${SEARCH_SETTINGS}`;
 
-       return fetch(URL)// возращаем данные промиса во внешний код
-            .then(response => response.json())
-           .then(data => {
-            //    console.log(data);
-            //    if (this.page === 1 && data.totalHits !== 0 ) {
-            //        imagesFoundMessage(data.totalHits);
-            //    }
-            //    if (data.totalHits === 0) {
-            //        noImagesFoundMessage();
-            //    }
-            //    if (this.page * 40 > data.totalHits && data.totalHits !== 0) {
-            //        endOfSearchMessage();
+        try {
+            const response = await fetch(URL);// возращаем данные промиса во внешний код
+            this.incrementPage();// если запрос успешный то увеличиваем страницу на 1
+           
+            return await response.json(); //возвращаем данные промиса во внешний код
+            
+        } catch (error) {
+            console.log(error.message);
+        }
 
-            //    }
-               this.incrementPage();// если запрос успешный то увеличиваем страницу на 1
-               return data.hits ; //возвращаем данные промиса во внешний код
-        });
     }
   
     incrementPage() {
@@ -51,3 +100,4 @@ export default class NewApi {
     }
 
 }
+
